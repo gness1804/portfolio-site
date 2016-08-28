337212d0aff45d674042826dc63843643b3f0d23
gness1804 <graham.nessler@gmail.com>
2016-08-27 20:35:20 -0600

    Improves dropdown menu by adding better margins and positioning

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ $(document).ready(function () {
       $(".testing").toggleClass("border-test");
     }
   });
-  // $(".dropdown-super-menu").hover(function () {
-  //   $(".dropdown-sub-menu").show();
-  // });
-  // $(".dropdown-sub-menu").mouseleave(function () {
-  //   $(".dropdown-sub-menu").hide();
-  // });
+  $(".dropdown-super-menu").hover(function () {
+    $(".dropdown-sub-menu").show();
+  });
+  $(".dropdown-sub-menu").mouseleave(function () {
+    $(".dropdown-sub-menu").hide();
+  });
   // $(".dropdown-super-menu").mouseleave(function () {
   //   $(".dropdown-sub-menu").hide();
   // });

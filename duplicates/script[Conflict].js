04f8afcd76516d0ce40b3e0b49e5562e9f15f09f
gness1804 <graham.nessler@gmail.com>
2016-08-29 11:04:37 -0600

    Adds file for duplicates

---
+++ duplicates/script[Conflict].js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(document).on("keydown", function(key) {
+    if (key.which === 66) { // "B"
+      $(".testing").toggleClass("border-test");
+    }
+  });
+  $(".nav-bar-list").hover(function () {
+      $(".dropdown-sub-menu").fadeIn(500);
+  });
+  $(".nav-bar-list").mouseleave(function () {
+    $(".dropdown-sub-menu").fadeOut(500);
+  });
+  // $(".dropdown-sub-menu").mouseleave(function () {
+  //   $(".dropdown-sub-menu").hide();
+  // });
+  // $(".dropdown-super-menu").hover(function () {
+  //   $(".dropdown-sub-menu").show();
+  // });
+}); //end of jQuery body

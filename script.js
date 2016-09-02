b4151561e508a9403da0952a72a7150b4f49f689
gness1804 <graham.nessler@gmail.com>
2016-09-01 20:13:49 -0600

    basic functionality of dropdown now works

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ $(document).ready(function () {
   });
   $(".dropdown-menu-actual").hide();
   $(".dropdown-menu-container").hover(function () {
-    $(this).children().show();
+    if (window.outerWidth > 875) {
+      $(this).children().fadeIn(500);
+    }
+  });
+  $(".dropdown-menu-container").mouseleave(function () {
+    $(".dropdown-menu-actual").fadeOut(500);
   });
   // $(".nav-bar-list").hover(function () {
   //     if (window.outerWidth > 875) {

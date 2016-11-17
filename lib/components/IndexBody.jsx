5c8f80bad1121e03b156e3d9c6241c992f0d4e91
gness1804 <graham.nessler@gmail.com>
2016-11-16 23:34:13 -0700

    adds some components to router

---
+++ lib/components/IndexBody.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ReactDOM = require('react-dom');
+
+const IndexBody = React.createClass({
+  render() {
+    return (
+      <div>Hello! This is a test. I am IndexBody.</div>
+    );
+  },
+});
+
+export default IndexBody;
---
+++ lib/components/ProjectMenu.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+const ReactDOM = require('react-dom');
+
+class ProjectMenu extends Component {
+
+  render() {
+    return (
+      <div>Hello! This is a test. I am Project Menu.</div>
+    );
+  }
+
+}
+
+
+export default ProjectMenu;

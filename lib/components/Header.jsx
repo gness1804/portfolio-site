0861ba8749ae83554ef8cdd18adcdccd6b7e3dca
gness1804 <graham.nessler@gmail.com>
2016-11-17 15:52:28 -0700

    reformats h1 link color to black

--- lib/components/Header.jsx
+++ lib/components/Header.jsx
@@ -9,7 +9,7 @@ class Header extends Component {
     return (
       <div>
         <header className="header-each-page">
-          <Link to="/">
+          <Link className="headline-link" to="/">
             <h1>Graham Nessler</h1>
           </Link>
           <img className="header-photo" src="images/skype-photo-2.jpg" alt="Photo of me in blue shirt." />

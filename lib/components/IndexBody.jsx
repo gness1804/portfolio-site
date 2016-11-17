26da43020449860e5c429451756ba9bee78c9def
gness1804 <graham.nessler@gmail.com>
2016-11-17 15:30:23 -0700

    page upper section is restored

--- lib/components/IndexBody.jsx
+++ lib/components/IndexBody.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+
 const ReactDOM = require('react-dom');
 
 class IndexBody extends Component {
 
   render() {
     return (
-      <div>I am the landing page material that lives in the Home page.</div>
+      <div>
+        <div className="page-upper-section">
+          <p>
+            I am a front-end software engineer, published author, and educator. Feel free to browse my projects below and to <a href="mailto:gnessler@umich.edu" target="_blank">get in touch</a>.
+          </p>
+        </div>
+      </div>
     );
   }
 

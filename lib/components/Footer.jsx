53af5cdca76ac376a55d14ee48169e575713ca5e
gness1804 <graham.nessler@gmail.com>
2016-11-17 12:20:32 -0700

    attaches actual header and footer to Application

--- lib/components/Footer.jsx
+++ lib/components/Footer.jsx
@@ -5,7 +5,22 @@ class Footer extends Component {
 
   render() {
     return (
-      <div>Hello! I am the footer.</div>
+      <div>
+        <footer className="footer-each-page">
+          <a href="https://www.linkedin.com/in/graham-nessler-b9025756" target="_blank">
+            <img src="images/linkedin.png" alt="LinkedIn icon." />
+          </a>
+          <a href="https://github.com/gness1804" target="_blank">
+            <img src="images/github.png" alt="GitHub logo" />
+          </a>
+          <a href="mailto:gnessler@umich.edu" target="_blank">
+            <img src="images/gmail.png" alt="Gmail logo." />
+          </a>
+          <a href="https://twitter.com/gness1804" target="_blank">
+            <img src="images/twitter.png" alt="Twitter icon." />
+          </a>
+        </footer>
+      </div>
     );
   }
 
--- lib/components/Header.jsx
+++ lib/components/Header.jsx
@@ -1,11 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 const ReactDOM = require('react-dom');
 
 class Header extends Component {
 
   render() {
     return (
-      <div>Hello! I am the header.</div>
+      <div>
+        <header className="header-each-page">
+          <h1>Graham Nessler</h1>
+          <img className="header-photo" src="images/skype-photo-2.jpg" alt="Photo of me in blue shirt." />
+          <nav className="nav-bar">
+            <section className="dropdown-menu-container">
+              <a href="index.html">Home</a>
+            </section>
+            <section className="dropdown-menu-container">
+              <a href="project-menu.html" className="selected">Projects</a>
+              <section className="dropdown-menu-actual projects-dropdown">
+                <a href="netwerker.html">Netwerker</a>
+                <a href="flash-shopper.html">Flash Shopper</a>
+                <a href="project-menu.html">Other Projects</a>
+              </section>
+            </section>
+            <section className="dropdown-menu-container">
+              <a href="background.html">About Me</a>
+              <section className="dropdown-menu-actual background-dropdown">
+                <a href="background.html#work-hist">Employment History</a>
+                <a href="background.html#education">Education</a>
+              </section>
+              </section>
+              <section className="dropdown-menu-container">
+                <a href="https://gness1804.github.io/Personal-Blog/">Blog</a>
+              </section>
+          </nav>
+        </header>
+      </div>
     );
   }
 

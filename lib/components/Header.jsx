a1835a036bdb007d35c7e385db8b1b0d7f9f8cc0
gness1804 <graham.nessler@gmail.com>
2016-11-18 11:33:32 -0500

    dropdown menu is functioning

--- lib/components/Header.jsx
+++ lib/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 const ReactDOM = require('react-dom');
+require('./dropdown');
 
 class Header extends Component {
 

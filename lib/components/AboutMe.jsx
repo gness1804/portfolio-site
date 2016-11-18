58a6aed67b5178c744a8b4fdbf7a80fcf97d4ded
gness1804 <graham.nessler@gmail.com>
2016-11-17 20:38:49 -0500

    got work hist section reconstructed

--- lib/components/AboutMe.jsx
+++ lib/components/AboutMe.jsx
@@ -5,9 +5,33 @@ class AboutMe extends Component {
 
   render() {
     return (
-      <div>I am the About Me page.
-        <div id="#work-hist">I am the work history section.</div>
-        <div id="#education">I am the education section.</div>
+      <div>
+        <section className="bio-page-headline">
+          <h2>Who Am I?</h2>
+        </section>
+        <section className="big-button-container">
+          <a href="resumes/Nessler-Resume-11-14-16.pdf" target="_blank">
+            <button type="button" className="big-button">View My Resume</button>
+          </a>
+        </section>
+        <section className="bio-page-section-container">
+          <article className="bio-page-section">
+            <h3 id="work-hist" target="_blank">Work History</h3>
+              <p>
+                <span>Selected Positions:</span>
+              </p><br></br>
+              <p>
+                Visiting Professor of History, <a href="https://www.fau.edu" target="_blank"><span>Florida Atlantic University</span></a> (2015-2016)
+              </p>
+              <p>
+                Post-Doctoral Fellow, <a href="https://www.bentley.edu" target="_blank"><span>Bentley University</span></a> (2013-2015)
+              </p>
+              <p>
+                Ad Interim Assistant Professor of History, <a href="https://www.tamuc.edu" target="_blank"><span>Texas A and M University-Commerce</span></a> (2012-2013)
+              </p><br></br>
+          </article>
+
+        </section>
       </div>
     );
   }

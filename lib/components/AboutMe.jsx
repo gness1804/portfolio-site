ce0813d0ba5292fd3aa7aad0b9baa3481fb822a4
gness1804 <graham.nessler@gmail.com>
2016-11-17 20:41:36 -0500

    finished about me page

--- lib/components/AboutMe.jsx
+++ lib/components/AboutMe.jsx
@@ -30,7 +30,18 @@ class AboutMe extends Component {
                 Ad Interim Assistant Professor of History, <a href="https://www.tamuc.edu" target="_blank"><span>Texas A and M University-Commerce</span></a> (2012-2013)
               </p><br></br>
           </article>
-
+          <article className="bio-page-section">
+            <h3 id="education">Education</h3>
+            <p>
+              Certificate in Front End Engineering, <a href="https://www.turing.io/" target="_blank"><span>Turing School of Software and Design</span></a> (anticipated in March 2017)
+            </p>
+            <p>
+              PhD, History, <a href="https://www.umich.edu/" target="_blank"><span>University of Michigan</span></a> (2011)
+            </p>
+            <p>
+              BA, History, <a href="http://www.wm.edu/" target="_blank"><span>College of William and Mary</span></a> (2005)
+            </p><br></br>
+          </article>
         </section>
       </div>
     );

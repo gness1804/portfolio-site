f38b37e0a304d21cdf13c6fb0ba60e060432c651
gness1804 <graham.nessler@gmail.com>
2017-01-17 11:20:07 -0700

    Add first part of FareHarbor page

--- lib/components/AboutMe.jsx
+++ lib/components/AboutMe.jsx
@@ -13,7 +13,7 @@ class AboutMe extends Component {
           <h2>Who Am I?</h2>
         </section>
         <section className="big-button-container">
-          <a href="resumes/Nessler-Resume-12-16.pdf" target="_blank">
+          <a href="resumes/Nessler-Resume-12-16.pdf" target="_blank" rel="noopener noreferrer">
             <button type="button" className="big-button">View My Resume</button>
           </a>
         </section>

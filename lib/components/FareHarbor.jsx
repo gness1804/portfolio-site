a07cfe774794f4ad65f28df9f129b17a3108af34
gness1804 <graham.nessler@gmail.com>
2017-01-17 10:22:53 -0700

    Add blank FareHarbor component and hook it up with router

---
+++ lib/components/FareHarbor.jsx
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+
+class FareHarbor extends Component {
+
+  render() {
+    return (
+      <div>
+        <section className="project-page-headline">
+          <h2>FareHarbor Toolkit</h2>
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <img src="images/github.png" alt="GitHub logo" title="Project GitHub link" />
+            <p>Project Link (Github)</p>
+          </a>
+        </section>
+        <section className="project-page-section">
+            <h3>Principal Accomplishments</h3>
+            <img src="" alt="Screenshot." />
+            <p>
+
+           </p>
+          </section>
+          <section className="project-page-section">
+              <h3>Tools Used</h3>
+              <img src="" alt="." />
+              <p>
+
+             </p>
+            </section>
+            <section className="project-page-section">
+                <h3>What I Learned</h3>
+                <img src="images/thinking.svg" alt="A thinking head." title="Image courtesy of Elias Bikbulatov for FlatIcon, http://www.flaticon.com" />
+                <p>
+                 
+               </p>
+          </section>
+      </div>
+    );
+  }
+
+}
+
+
+export default FareHarbor;
--- lib/components/IndexBody.jsx
+++ lib/components/IndexBody.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-const ReactDOM = require('react-dom');
-
 class IndexBody extends Component {
 
   render() {
@@ -10,7 +8,7 @@ class IndexBody extends Component {
       <div>
         <div className="page-upper-section">
           <p>
-            I am a front-end software engineer, published author, and educator. Feel free to browse my projects below and to <a href="mailto:gnessler@umich.edu" target="_blank">get in touch</a>.
+            I am a front-end software engineer, published author, and educator. Feel free to browse my projects below and to <a href="mailto:gnessler@umich.edu" target="_blank" rel="noopener noreferrer">get in touch</a>.
           </p>
         </div>
         <section className="two-article-control">
@@ -18,24 +16,24 @@ class IndexBody extends Component {
           <Link to="/netwerker">
             <h3>Netwerker</h3>
           </Link>
-           <a href="https://github.com/gness1804/netwerker" target="_blank">
+           <a href="https://github.com/gness1804/netwerker" target="_blank" rel="noopener noreferrer">
              <h4>(GitHub)</h4>
            </a>
            <img src="images/netwerker.png" alt="Screen shot from Netwerker." />
            <p>
-             <a href="https://netwerker-3d622.firebaseapp.com/" target="_blank">Netwerker</a> is a professional networking app, created with Matt Kaufman and Christine Gamble, that allows the user to store contacts, sort them alphabetically, delete them, flag them for followup, and add images. It was created using the toolbox of React, Firebase, Webpack, and Enzyme. For more details, see the <Link to="/netwerker">project page.</Link>
+             <a href="https://netwerker-3d622.firebaseapp.com/" target="_blank" rel="noopener noreferrer">Netwerker</a> is a professional networking app, created with Matt Kaufman and Christine Gamble, that allows the user to store contacts, sort them alphabetically, delete them, flag them for followup, and add images. It was created using the toolbox of React, Firebase, Webpack, and Enzyme. For more details, see the <Link to="/netwerker">project page.</Link>
            </p>
          </article>
          <article className="main-article-half-width proj2">
           <Link to="/flashshopper">
             <h3>Flash Shopper</h3>
           </Link>
-           <a href="https://github.com/gness1804/grocery-list-react" target="_blank">
+           <a href="https://github.com/gness1804/grocery-list-react" target="_blank" rel="noopener noreferrer">
              <h4>(GitHub)</h4>
            </a>
            <img src="images/flash-shopper.png" alt="Screenshot." />
            <p>
-             <a href="https://github.com/gness1804/grocery-list-redux/tree/master" target="_blank">Flash Shopper</a> is an application created by me independent of the Turing School curriculum. It is a grocery list app where users can enter items, sort them, delete them, and edit them. The app also populates aisles by category (currently available for 1 location). This app aims to present a smart and sleek alternative to existing grocery apps. For more details, see the <Link to="/flashshopper">project page.</Link>
+             <a href="https://github.com/gness1804/grocery-list-redux/tree/master" target="_blank" rel="noopener noreferrer">Flash Shopper</a> is an application created by me independent of the Turing School curriculum. It is a grocery list app where users can enter items, sort them, delete them, and edit them. The app also populates aisles by category (currently available for 1 location). This app aims to present a smart and sleek alternative to existing grocery apps. For more details, see the <Link to="/flashshopper">project page.</Link>
            </p>
          </article>
         </section>
@@ -50,10 +48,10 @@ class IndexBody extends Component {
          <article className="page-lower-section">
            <h3>Who Am I?</h3>
            <p>
-             My web development experience includes projects using React, Redux, JavaScript/jQuery, SASS/CSS, and other technologies. I am also the author of a book on the only successful slave revolution in world history, entitled <a href="http://uncpress.unc.edu/books/12448.html" target="_blank">An Islandwide Struggle for Freedom: Revolution, Emancipation, and Reenslavement in Hispaniola, 1789-1809.</a> For more information, see my <Link to="/aboutme">"About Me" page.</Link>
+             My web development experience includes projects using React, Redux, JavaScript/jQuery, SASS/CSS, and other technologies. I am also the author of a book on the only successful slave revolution in world history, entitled <a href="http://uncpress.unc.edu/books/12448.html" target="_blank" rel="noopener noreferrer">An Islandwide Struggle for Freedom: Revolution, Emancipation, and Reenslavement in Hispaniola, 1789-1809.</a> For more information, see my <Link to="/aboutme">"About Me" page.</Link>
            </p>
          </article>
-         <a href="http://uncpress.unc.edu/books/12448.html" target="_blank">
+         <a href="http://uncpress.unc.edu/books/12448.html" target="_blank" rel="noopener noreferrer">
            <figure id="fig-main">
              <img src="images/book_cover.jpg" alt="The cover of my book." title="Book Cover" id="book-img" />
              <figcaption>My book</figcaption>

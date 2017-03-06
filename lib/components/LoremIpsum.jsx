a868c21f6ac52d485b30a76af15b3b34daae8399
gness1804 <graham.nessler@gmail.com>
2017-03-06 10:52:47 -0700

    Add lorem ipsum generator

--- lib/components/LoremIpsum.jsx
+++ lib/components/LoremIpsum.jsx
@@ -6,33 +6,26 @@ class LoremIpsum extends Component {
     return (
       <div>
         <section className="project-page-headline">
-          <h2>Title</h2>
-          <a href="" target="_blank">
+          <h2>Lorem Ipsum Generator</h2>
+          <a href="https://github.com/gness1804/160gr8-lorem-ipsum-gen" target="_blank" rel="noreferrer noopener">
             <img src="images/github.png" alt="GitHub logo" title="Project GitHub link" />
             <p>Project Link (Github)</p>
           </a>
         </section>
         <section className="project-page-section">
             <h3>Principal Accomplishments</h3>
-            <img src="" alt="Screenshot." />
+            <img src="images/lorem-ipsum-screenshot.png" alt="Screenshot." />
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              This is a fun side project that generates random lorem ipsum related to my cohort at Turing School. It allows the user to specify a word count, add a word to the generator, easily copy text to clipboard via a button, and clear text. It is a useful application to generate humorous lorem ipsum text. Check it out <a href="https://gness1804.github.io/160gr8-lorem-ipsum-gen/" target="_blank" rel="noopener noreferrer">live!</a>
            </p>
           </section>
           <section className="project-page-section">
-              <h3>Tools Used</h3>
-              <img src="" alt="" />
-              <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-             </p>
-            </section>
-            <section className="project-page-section">
-                <h3>What I Learned</h3>
-                <img src="images/thinking.svg" alt="A thinking head." title="Image courtesy of Elias Bikbulatov for FlatIcon, http://www.flaticon.com" />
-                <p>
-                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-               </p>
-          </section>
+            <h3>What I Learned</h3>
+            <img src="images/thinking.svg" alt="A thinking head." title="Image courtesy of Elias Bikbulatov for FlatIcon, http://www.flaticon.com" />
+            <p>
+             This project uses an array of words for the text. I learned a lot about array methods and how to manipulate arrays. This project also enhanced my knowledge of jQuery, since DOM manipuation was a key part of the project.
+           </p>
+        </section>
       </div>
     );
   }
